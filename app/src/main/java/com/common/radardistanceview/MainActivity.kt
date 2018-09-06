e6f50d5df2f714f27a7e3f0b670b227c2746bea5
kirill.stoianov <kirillstoianov46@gmail.com>
2018-09-06 18:06:29 +0300

    Added image scaling. Added radar grid lines. OnWork: pulsing circle animation

--- app/src/main/java/com/common/radardistanceview/MainActivity.kt
+++ app/src/main/java/com/common/radardistanceview/MainActivity.kt
@@ -2,8 +2,8 @@ package com.common.radardistanceview
 
 import android.animation.ValueAnimator
 import android.os.Bundle
-import android.os.Handler
 import android.support.v7.app.AppCompatActivity
+import android.widget.SeekBar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,14 +12,42 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        radar.post {
-            ValueAnimator.ofFloat(0f, 1f).apply {
-                duration = 3000
-                addUpdateListener {
-                    radar.setAngle(it.animatedValue as Float)
-                }
-                start()
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
             }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                radarView.post {
+                    val maxProgress = seekBar.max / 100f
+                    val currentProgress = progress / 100f
+                    val angle = maxProgress - currentProgress
+                    radarView.setScaleFactor(angle)
+                }
+            }
+        })
+
+        radarView.setScaleFactor(seekBar.max / 100f)
+
+
+
+
+        ValueAnimator.ofFloat(0f, 100f).apply {
+            addUpdateListener {
+                val animatedValue = it.animatedValue as Float
+                if (animatedValue == 100f) {
+                    radarView.setPulsingScaleFactor(1 / 100f)
+                } else {
+                    radarView.setPulsingScaleFactor(animatedValue / 100f)
+                }
+            }
+            duration = 5000
+            repeatMode = ValueAnimator.RESTART
+            repeatCount = ValueAnimator.INFINITE
+            start()
         }
     }
 }
--- app/src/main/java/com/common/radardistanceview/RadarDistanceView.kt
+++ app/src/main/java/com/common/radardistanceview/RadarDistanceView.kt
@@ -12,6 +12,10 @@ import android.view.View
  */
 class RadarDistanceView(context: Context, attributeSet: AttributeSet?, deff: Int) : View(context, attributeSet, deff) {
 
+    companion object {
+        val TAG: String = RadarDistanceView::class.java.simpleName
+    }
+
     constructor(context: Context) : this(context, null, 0)
     constructor(context: Context, attributeSet: AttributeSet?) : this(context, attributeSet, 0)
 
@@ -19,18 +23,11 @@ class RadarDistanceView(context: Context, attributeSet: AttributeSet?, deff: Int
         setLayerType(LAYER_TYPE_HARDWARE, null)
     }
 
-    companion object {
-        val TAG: String = RadarDistanceView::class.java.simpleName
-    }
+    private var scaleFactor = 0.0f
 
-    private var angle = 0.0f
+    private val gradientStrokeWidth = 15F
 
-    fun setAngle(value: Float) {
-        angle = value
-        invalidate()
-    }
-
-    val resource: Bitmap by lazy {
+    private val resourceBitmap: Bitmap by lazy {
         BitmapFactory.decodeResource(context.resources, R.drawable.img_kasha_radar)
     }
 
@@ -43,12 +40,10 @@ class RadarDistanceView(context: Context, attributeSet: AttributeSet?, deff: Int
             val mShader = SweepGradient(width / 2F, height / 2F, colorStateList, floatArrayOf(0f, .2f, .5f, .7f, 1f))
 
             isAntiAlias = true
-
             color = Color.BLACK
             shader = mShader
         }
     }
-
 
     private val linePaint by lazy {
         Paint().apply {
@@ -56,13 +51,21 @@ class RadarDistanceView(context: Context, attributeSet: AttributeSet?, deff: Int
             color = lineColor
             style = Paint.Style.FILL_AND_STROKE
             isAntiAlias = true
-
             strokeWidth = 4f
         }
     }
-    private val gradientStrokeWidth = 15F
 
-    private val transparentCircle by lazy {
+    private val pulsingCirclePaint by lazy {
+        Paint().apply {
+            val lineColor: Int = ContextCompat.getColor(context, R.color.colorLine)
+            color = lineColor
+            style = Paint.Style.FILL_AND_STROKE
+            isAntiAlias = true
+            strokeWidth = 4f
+        }
+    }
+
+    private val transparentCirclePaint by lazy {
         Paint().apply {
             color = Color.TRANSPARENT
             isAntiAlias = true
@@ -71,24 +74,33 @@ class RadarDistanceView(context: Context, attributeSet: AttributeSet?, deff: Int
         }
     }
 
-    override fun onDraw(canvas: Canvas) {
-        super.onDraw(canvas)
-        val width: Float = canvas.width.toFloat()
-        val height: Float = canvas.height.toFloat()
-
-        drawGradientStroke(width, height, canvas)
-        drawImage(canvas)
-
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        setMeasuredDimension(Math.min(measuredWidth, measuredHeight), Math.min(measuredWidth, measuredHeight))
     }
 
-    private fun drawLines(width: Float, height: Float, canvas: Canvas) {
-        canvas.drawLine(width / 3, 0f, width / 3, height, linePaint)
-        canvas.drawLine(0f, height / 3, width, height / 3, linePaint)
-        canvas.drawLine(width / 1.5f, 0f, width / 1.5f, height, linePaint)
-        canvas.drawLine(0f, height / 1.5f, width, height / 1.5f, linePaint)
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        drawGradientStroke(canvas)
+        drawImage(canvas)
+    }
+
+    fun setScaleFactor(value: Float) {
+        scaleFactor = value
+        invalidate()
+    }
+
+    private fun drawGradientStroke(canvas: Canvas) {
+        val width: Float = canvas.width.toFloat()
+        val height: Float = canvas.height.toFloat()
+        val radius = Math.min(width, height) / 2
+        canvas.drawCircle(width / 2, height / 2, radius, mainGradientStrokePaint)
+        canvas.drawCircle(width / 2, height / 2, radius - gradientStrokeWidth, transparentCirclePaint)
     }
 
     private fun drawImage(mainCanvas: Canvas) {
+
         val size = (Math.min(mainCanvas.width, mainCanvas.height) - gradientStrokeWidth)
 
         val output = Bitmap.createBitmap(size.toInt(), size.toInt(), Bitmap.Config.ARGB_8888)
@@ -97,40 +109,83 @@ class RadarDistanceView(context: Context, attributeSet: AttributeSet?, deff: Int
 
         val color = Color.CYAN
         val paint = Paint()
-        val rect = Rect(0, 0, size.toInt(), size.toInt())
-
         paint.isAntiAlias = true
         canvas.drawARGB(0, 0, 0, 0)
         paint.color = color
 
-        canvas.drawCircle(size / 2f, size / 2f,
-                size / 2f, paint)
+        canvas.drawCircle(size / 2f, size / 2f, size / 2f, paint)
 
         paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
-        val originalWidth = resource.width
-        val originalHeight = resource.height
 
-        val scale = (size / originalWidth) + angle
+        val originalWidth = resourceBitmap.width
+        val originalHeight = resourceBitmap.height
 
-        val xTranslation = 0.0f
-        val yTranslation = (size - originalHeight * scale) / 2.0f
+        val scaleX = (size / originalWidth) + scaleFactor
+        val scaleY = (size / originalHeight) + scaleFactor
+
+        val xTranslation = (size - originalWidth * scaleX) / 2.0f
+        val yTranslation = (size - originalHeight * scaleY) / 2.0f
 
         val transformation = Matrix().apply {
             postTranslate(xTranslation, yTranslation)
-            preScale(scale, scale)
+            preScale(scaleX, scaleY)
         }
 
-        canvas.drawBitmap(resource, transformation, paint)
+        canvas.drawBitmap(resourceBitmap, transformation, paint)
 
-        drawLines(size, size, canvas)
+        drawLines(canvas, size)
+        drawAlphaCircle(canvas)
 
         val xy = (Math.min(mainCanvas.width, mainCanvas.height) / 2) - (size / 2)
         mainCanvas.drawBitmap(output, xy, xy, Paint())
     }
 
-    private fun drawGradientStroke(width: Float, height: Float, canvas: Canvas) {
-        val radius = Math.min(width, height) / 2
-        canvas.drawCircle(width / 2, height / 2, radius, mainGradientStrokePaint)
-        canvas.drawCircle(width / 2, height / 2, radius - gradientStrokeWidth, transparentCircle)
+    private fun drawLines(canvas: Canvas, circleDiameter: Float) {
+        drawLineInsideCircle(canvas = canvas, segmentHeight = circleDiameter / 2, diameter = circleDiameter)
+        drawLineInsideCircle(canvas = canvas, segmentHeight = circleDiameter / 4, diameter = circleDiameter, invert = true)
+
     }
+
+    private fun drawLineInsideCircle(canvas: Canvas, segmentHeight: Float, diameter: Float, drawX: Boolean = true, drawY: Boolean = true, invert: Boolean = false) {
+        val aAngle = Math.acos(1 - (2 * (segmentHeight)).toDouble() / diameter)
+        val chordLength = diameter * Math.sin(aAngle)
+        val padding = ((width - chordLength) / 2).toFloat()
+
+        val startPosition = chordLength.toFloat() + padding - gradientStrokeWidth
+
+        //draw horizontal line
+        if (drawX) canvas.drawLine(padding, segmentHeight, startPosition, segmentHeight, linePaint)
+
+        //draw vertical line
+        if (drawY) canvas.drawLine(segmentHeight, padding, segmentHeight, startPosition, linePaint)
+
+        if (invert) {
+            //draw invert horizontal line
+            if (drawX) canvas.drawLine(padding, segmentHeight + diameter / 2, startPosition, segmentHeight + diameter / 2, linePaint)
+
+            //draw invert vertical line
+            if (drawY) canvas.drawLine(segmentHeight + diameter / 2, padding, segmentHeight + diameter / 2, startPosition, linePaint)
+        }
+    }
+
+    //----------------------------------------------------------------------
+    //todo testing pulse animation
+    var pulsingCircleScaleFactor: Float = 0f
+
+    fun setPulsingScaleFactor(value: Float) {
+        pulsingCircleScaleFactor = value
+        invalidate()
+    }
+
+    //todo testing pulse animation
+    private fun drawAlphaCircle(canvas: Canvas) {
+        val newRadius = canvas.width * pulsingCircleScaleFactor
+
+        if (newRadius > (canvas.width / 2 - gradientStrokeWidth)) {
+        } else {
+            pulsingCirclePaint.alpha = 200 - (255 * pulsingCircleScaleFactor).toInt()
+            canvas.drawCircle(canvas.width / 2f, canvas.height / 2f, newRadius, pulsingCirclePaint)
+        }
+    }
+
 }
